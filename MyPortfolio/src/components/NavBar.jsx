33abc1fe3e228983b2fabec0164d9d571beff36c
Guillermo Alejandro Costilla <gcostilla96@gmail.com>
2023-10-26 11:35:56 -0300

    cv agregado

--- MyPortfolio/src/components/NavBar.jsx
+++ MyPortfolio/src/components/NavBar.jsx
@@ -90,6 +90,17 @@ export default function Nav() {
                         </svg>
                     </Link>
                 </NavbarItem>
+                <NavbarItem>
+                    <Link className="font-bold text-gray-100 hover:text-sky-700" href={cv} download={"CostillaGuillermo.pdf"} target="_blank" rel="noopener noreferrer">
+                        Cv<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-file-cv" width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#00abfb" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <path d="M14 3v4a1 1 0 0 0 1 1h4" />
+                            <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z" />
+                            <path d="M11 12.5a1.5 1.5 0 0 0 -3 0v3a1.5 1.5 0 0 0 3 0" />
+                            <path d="M13 11l1.5 6l1.5 -6" />
+                        </svg>
+                    </Link>
+                </NavbarItem>
             </NavbarContent>
 
             <NavbarMenu>
