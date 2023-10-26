72baf6f9e47ea4df750a774bc8d942ac08fb8acb
Guillermo Alejandro Costilla <gcostilla96@gmail.com>
2023-10-26 11:22:16 -0300

    PDF

--- MyPortfolio/src/components/NavBar.jsx
+++ MyPortfolio/src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 import '../styles/perfil.css'
-import cv from '../assets/Costilla Guillermo.pdf'
+import cv from '../assets/CostillaGuillermo.pdf'
 import { Navbar, NavbarMenu, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuItem, NavbarMenuToggle, Link } from "@nextui-org/react";
 
 export default function Nav() {
@@ -30,11 +30,16 @@ export default function Nav() {
             </svg></>
         },
         {
-            title: "Curriculum Vitae", href: { cv }, icon: <><svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-user" width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#00abfb" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
-                <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
-            </svg></>
+            title: "Curriculum Vitae",
+            href: cv, // Usamos la variable que contiene el enlace al archivo PDF
+            download: "CostillaGuillermo.pdf", // Nombre del archivo a descargar
+            icon: (
+                <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-user" width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#00abfb" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
+                    <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+                </svg>
+            )
         }
     ];
 
